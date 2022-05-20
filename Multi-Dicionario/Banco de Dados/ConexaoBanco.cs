5522c97a0c2aef873c74a9388421ad5ae1c03806
Luiz Silva <lfdjs0000@gmail.com>
2022-05-20 11:11:37 -0300

    Atualização Mysql Connector

--- Multi-Dicionario/Banco de Dados/ConexaoBanco.cs
+++ Multi-Dicionario/Banco de Dados/ConexaoBanco.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
-using System.Data;
+﻿using System.Data;
+using MySqlConnector;
 
 namespace Multi_Dicionario.Banco_de_Dados
 {
